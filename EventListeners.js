9b57986fd05308653f312e75772229fa1c7c0fd6
developer-Manthan <patelmanthan0103@gmail.com>
2023-02-13 15:10:23 +0530

    Almost final commit

--- EventListeners.js
+++ EventListeners.js
@@ -1,29 +1,82 @@
 const hamButton = document.getElementById('ham-button');
 const dropdownContent = document.getElementById('dropdown-id');
-const subDropdownContent = document.getElementById('sub-dropdown-id');
-const subButton = document.getElementById('sub-button');
+
+//
+const subDropdownContent1 = document.getElementById('sub-dropdown-id-1');
+const subButton1 = document.getElementById('sub-button-1');
+
+const subDropdownContent2 = document.getElementById('sub-dropdown-id-2');
+const subButton2 = document.getElementById('sub-button-2');
+
+const subDropdownContent3 = document.getElementById('sub-dropdown-id-3');
+const subButton3 = document.getElementById('sub-button-3');
+
+const toggleClass = "sub-dropdown-content-show";
+
+//
+const bellButton = document.getElementById('bell-icon-button');
 
 const bellId = document.getElementById('bell-id');
 const alertContentId = document.getElementById('alert-content-id');
 const alertClass = document.getElementsByClassName('alert-class');
 
+//
 hamButton.addEventListener("click", function() {
     dropdownContent.classList.toggle("dropdown-content-show");
 });
 
-subButton.addEventListener("click", function() {
-    subDropdownContent.classList.toggle('sub-dropdown-content-show');
+subButton1.addEventListener("click", function() {
+    
+    subDropdownContent1.classList.toggle(toggleClass);
+
+    if(subDropdownContent2.classList.contains(toggleClass)) {
+        subDropdownContent2.classList.toggle(toggleClass);
+    } else if(subDropdownContent3.classList.contains(toggleClass)) {
+        subDropdownContent3.classList.toggle(toggleClass);
+    }
+
+    
 });
+
+subButton2.addEventListener("click", function() {
+
+    if(subDropdownContent1.classList.contains(toggleClass)) {
+        subDropdownContent1.classList.toggle(toggleClass);
+    } else if(subDropdownContent3.classList.contains(toggleClass)) {
+        subDropdownContent3.classList.toggle(toggleClass);
+    }
+
+    subDropdownContent2.classList.toggle(toggleClass);
+});
+
+subButton3.addEventListener("click", function() {
+
+    if(subDropdownContent2.classList.contains(toggleClass)) {
+        subDropdownContent2.classList.toggle(toggleClass);
+    } else if(subDropdownContent1.classList.contains(toggleClass)) {
+        subDropdownContent1.classList.toggle(toggleClass);
+    }
+
+    subDropdownContent3.classList.toggle(toggleClass);
+});
+
+bellButton.addEventListener("mouseover", () => {
+    alertContentId.style.display = 'flex';
+})
+
+bellButton.addEventListener("mouseout", () => {
+    alertContentId.style.display = 'none';
+})
 
 // alertClass.addEventListener("mouseover", function() {
 //     alertContentId.style.display = 'block';
 // })
 
-[bellId, alertContentId].forEach(function(element) {
-    element.addEventListener("mouseover", function() {
-        alertContentId.style.display = 'block';
-    })
-});
+// [bellId, alertContentId].forEach(function(element) {
+//     element.addEventListener("mouseover", function() {
+//         alertContentId.style.display = 'block';
+//     })
+// });
 
 // let open = false;
 
@@ -44,13 +97,13 @@ subButton.addEventListener("click", function() {
 //     }, 300);
 // })
 
-[bellId, alertContentId].forEach(function(element) {
-    element.addEventListener("mouseleave", function() {
-        setTimeout(() => {
-            alertContentId.style.display = 'none';
-        }, 300);
-    })
-});
+// [bellId, alertContentId].forEach(function(element) {
+//     element.addEventListener("mouseleave", function() {
+//         setTimeout(() => {
+//             alertContentId.style.display = 'none';
+//         }, 300);
+//     })
+// });
 
 
 // subDropdownContent.onmouseover = function() {
---
+++ index.js
@@ -0,0 +1,65 @@
+const myRequest = new Request('dashboard.json');
+
+const navItems = document.querySelectorAll('.nav-item');
+const courseHeaderItems = document.querySelectorAll('.course-header-item');
+const showing = document.querySelector('.showing');
+const cardLine1 = document.querySelectorAll('.card-content .line-1');
+const cardLine3 = document.querySelectorAll('.card-content .line-3');
+const cardLine5 = document.querySelectorAll('.card-content .line-5');
+const subjects = document.querySelectorAll('#subject');
+const grades = document.querySelectorAll('#grade');
+const aboutUs = document.querySelector('.about-us');
+const footerText = document.querySelector('#footer-text');
+const cardImages = document.querySelectorAll('.card-img img');
+
+fetch(myRequest)
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    //
+    for(let i = 0; i<5; i++) {
+        navItems[i].innerHTML = data.nav_item[i];
+    }
+
+    //
+    for(let i = 0; i < 2; i++) {
+        let inText = document.createTextNode(data.course_header_item[i]);
+        courseHeaderItems[i].appendChild(inText);
+    }
+
+    //
+    showing.innerHTML = data.showing;
+
+    //
+    for(let [i, element] of cardLine1.entries()) {
+        element.innerHTML = data.cards[i].line_1;
+    }
+
+    for(let [i, element] of cardLine3.entries()) {
+        element.innerHTML = data.cards[i].line_3;
+    }
+
+    for(let [i, element] of cardLine5.entries()) {
+        element.innerHTML = data.cards[i].line_5;
+    }
+
+    for(let [i, element] of subjects.entries()) {
+        element.innerHTML = data.cards[i].subject;
+    }
+
+    for(let [i, element] of grades.entries()) {
+        element.innerHTML = data.cards[i].grade;
+    }
+
+    //
+    for(let [i, element] of cardImages.entries()) {
+        cardImages[i].src = data.cards[i].image;
+    }
+
+    //
+    aboutUs.innerHTML = data.about_us;
+
+    //
+    footerText.innerHTML = data.footer_text;
+})
